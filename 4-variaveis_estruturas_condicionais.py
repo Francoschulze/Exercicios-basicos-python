8fa2417fa87147d5fb5e0d42db5cf8c93a0b74f3
Franco <francoshculze@gmail.com>
2022-06-20 16:09:10 -0300

    Exercício 4

---
+++ 4-variaveis_estruturas_condicionais.py
@@ -0,0 +1,24 @@
+"""
+Escreva um programa que solicite o nome, o sobrenome e o salário atual
+de um funcionário e que calcule seu novo salário considerando cenários
+hipotéticos, com os seguintes aumentos: 10%, 25%, 30% e 50%. A
+mensagem de retorno deverá seguir o seguinte padrão
+    "Olá, [nome] [sobrenome]"
+    "Seu salário atual é : [salário]"
+    "Seu salário com 10% de aumento é: [salário]"
+    "Seu salário com 25% de aumento é: [salário]"
+    "Seu salário com 30% de aumento é: [salário]"
+    "Seu salário com 50% de aumento é: [salário]"
+"""
+
+nome = input('Digite seu nome: ')
+sobrenome = input('Digite seu sobrenome: ')
+salario_atual = float(input('Digite seu salário atual: '))
+
+# Utilizamos o \n para que o código faça a quebra de linha.
+print(f'Olá, {nome} {sobrenome}!\n'
+      f'Seu salário atual é: R$ {salario_atual:.2f}\n'
+      f'Seu salário com 10% de aumento será: R$ {salario_atual * 1.1:.2f}\n'
+      f'Seu salário com 25% de aumento será: R$ {salario_atual * 1.25:.2f}\n'
+      f'Seu salário com 30% de aumento será: R$ {salario_atual * 1.3:.2f}\n'
+      f'Seu salário com 50% de aumento será: R$ {salario_atual * 1.5:.2f}\n')
