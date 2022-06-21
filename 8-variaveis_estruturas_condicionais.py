d714d14c8b6e04c034ad7328faa27949372a1a99
Franco <francoshculze@gmail.com>
2022-06-21 09:13:45 -0300

    Exercício 8

---
+++ 8-variaveis_estruturas_condicionais.py
@@ -0,0 +1,10 @@
+"""
+Escreva um programa que gere um número aleatório entre 1 e 100.
+Dica: utilize o módulo random.
+"""
+
+# importando o método randint do módulo random nativo do python
+from random import randint
+
+# Será gerado um número aleatório entre 1 e 100.
+print(randint(1, 100))
