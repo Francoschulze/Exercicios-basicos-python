229f5e205e595aa9ce9c2114bc23635d82d5341d
Franco <francoshculze@gmail.com>
2022-06-21 09:16:56 -0300

    Exercício 9

---
+++ 9-variaveis_estruturas_condicionais.py
@@ -0,0 +1,9 @@
+"""
+Escreva um programa que informe o tamanho de uma string digitada.
+"""
+
+# var == variável
+var = input('Digite alguma coisa: ')
+
+# Método len() do python informa o tamanho da string.
+print(len(var))
